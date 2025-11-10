b3944706e2f50b1724bc6b1aafbb58c81dd45fb6
myriam-neuro <myriam.hamon@hotmail.fr>
2025-11-10 17:40:33 +0100

    minor server changes (glms)

--- scripts/submit_array_job.sh
+++ scripts/submit_array_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory containing files
-input_dir="/scratch/mhamon/"
+input_dir="/scratch/mhamon/data"
 output_dir="/scratch/mhamon/results/"
 
 # Create an array of file names
@@ -12,4 +12,4 @@ file_list="nwb_list.txt"
 printf "%s\n" "${files[@]}" > "$file_list"
 
 # Submit Slurm array job
-sbatch --array=0-$((${#files[@]}-1)) run_single_units_analysis.sh "$file_list" "$output_dir"
+sbatch --array=0-$((${#files[@]}-1))%30 run_single_units_analysis.sh "$file_list" "$output_dir"
